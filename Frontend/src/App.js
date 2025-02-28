b8ca51e69d12fde9dc3e93d8b24612e095ba0420
Gautam_yedla <gautamyedla@gmail.com>
2025-02-28 07:03:50 +0530

    your commit message

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 
 
 const App = () => (
